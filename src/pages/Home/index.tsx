f7cbcb8273e9667a23e14150120e5dec1b7027e7
rhuanlucass <rhuanfernandes.rf@gmail.com>
2024-06-11 09:41:05 -0300

    :sparkles: feat: Utilização de react-hook-form integrado com zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,16 +9,45 @@ import {
   TaskInput,
 } from "./styles";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
 export function Home() {
+  const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod
+      .number()
+      .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
+      .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
+  });
+
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  const handleCreateOnCycle = (data) => {
+    console.log(data);
+  };
+
+  const task = watch("task");
+  // Variável auxiliar
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateOnCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
             id="task"
+            {...register("task")}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -34,6 +63,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register("minutesAmount")}
           />
 
           <span>minutos.</span>
@@ -47,7 +77,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
